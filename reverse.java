2d77b6456857d81374d2ee2563ff35bedbf05119
saniya kushwaha <saniyakushwaha59@gmail.com>
2025-04-20 12:46:22 +0530

    msg

--- reverse.java
+++ reverse.java
@@ -3,8 +3,16 @@ public class reverse {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        
-    }
-    
+        System.out.print("enter size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("enter elements");
+        for(int i=0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("reversed array");
+        for(int i = n-1 ; i>=0 ; i--){
+            System.out.print(arr[i] + " ");
+        }
+ }
 }
